ba54ca60b6f7ad838b19f9140c9e7c99d65b7559
John Stegmaier <jupdown@protonmail.com>
2025-07-27 17:37:27 -0400

    Fix the horrible cheese speed... who the FUCK did this?

--- Scripts/CheeseEmitter.gd
+++ Scripts/CheeseEmitter.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 @export var cheese_scene: PackedScene
-@export var emit_interval: float = 0.006
+@export var emit_interval: float = 0.06
 @export var arc_speed_range := Vector2(500, 1050)
 @export var arc_angle_range := Vector2(10, -50) # slight randomness
 
