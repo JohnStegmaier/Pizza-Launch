5c3e74a176dd30865fea208f08346298bf46bb7d
John Stegmaier <jupdown@protonmail.com>
2025-07-30 02:11:46 -0400

    Add some really janky logic that allows the pizza to be thrown in the air

--- Scripts/PizzaGuyMove.gd
+++ Scripts/PizzaGuyMove.gd
@@ -1,7 +1,10 @@
 extends Node2D
-
+@onready var pizza := get_node("../Pizza")
 @export var speed := 600
 
 func _process(delta):
 	var direction := Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	position.x += direction * speed * delta
+	if pizza:
+		if(pizza.is_in_idle_state):
+			pizza.position.x += (direction * speed * delta)
